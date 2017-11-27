5dea10bca5e30d81e9f67258cb4b867c813c626f
ramzi27 <samram123>
2017-11-27 14:47:05 +0200

    upload

--- src/crossword/Const.java
+++ src/crossword/Const.java
@@ -9,7 +9,11 @@ public class Const { //contains constants
     public static final String words="words.fxml";
     public static final List<Crossword.Catagory> topicsArray= Arrays.asList(Crossword.Catagory.Animal, Crossword.Catagory.Health, Crossword.Catagory.Sport, Crossword.Catagory.Technology);
     public static final String stageTitle="Crossword Puzzle";
-    public static final char blank = ' ';
+    public static final char white = '1';
+    public static final char black = '0';
+
+
+
 
 
 }
