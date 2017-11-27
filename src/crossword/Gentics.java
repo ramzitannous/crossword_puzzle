e21cb019f88fe9200ee5042d9ecffb3c64aed420
ramzi27 <samram123>
2017-11-27 15:23:49 +0200

    upload

--- src/crossword/Gentics.java
+++ src/crossword/Gentics.java
@@ -1,14 +1,16 @@
 package crossword;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Gentics {
-    private char[][] chromosome;
+    private Character[][] chromosome;
     private int N, M, blanks;
     private ArrayList<Crossword> crosswords;
+    private ArrayList<Character[][]> chromosomes = new ArrayList<>();
 
-    public Gentics(char[][] chromosome, int n, int m, int blanks) {
+    public Gentics(Character[][] chromosome, int n, int m, int blanks) {
         this.chromosome = chromosome;
         N = n;
         M = m;
@@ -16,31 +18,38 @@ public class Gentics {
         crosswords = Save.getCrossWords();
     }
 
-    public char[][] getRandomChromo() {
+    public Character[][] getRandomChromo() {
         int lenCount = 0;
         for (int i = 0; i < crosswords.size(); i++) {
             lenCount += crosswords.get(i).getLen();
         }
         if (N * M - blanks < lenCount)
             return null;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (chromosome[i][j] != Const.black)
-                    chromosome[i][j] = generateRandom();
-
+        for (int i = 0; i < crosswords.size(); i++) {
+            final char[] chars = crosswords.get(i).getWord().toCharArray();
+            for (int j = 0; j < crosswords.get(i).getLen(); j++) {
+                generateRandom(chars, j);
             }
         }
         return chromosome;
     }
 
-    public char generateRandom() {
-
-        int i, j;
-        i = new Random().nextInt(crosswords.size());
-        String s = crosswords.get(i).getWord();
-        j = new Random().nextInt(crosswords.get(i).getLen());
-        return s.charAt(j);
+    public void generateRandom(char[] chars, int j) {
+        int k = new Random().nextInt(N);
+        int l = new Random().nextInt(M);
+        if (chromosome[k][l] == Const.white)//not black and doesn't have a letter
+            chromosome[k][l] = chars[j];
+        else
+            generateRandom(chars, j);
     }
 
+    public void generateSolutions() {
+        for (int i = 0; i < 10; i++) {
+            chromosomes.add(getRandomChromo());
+            for (int j = 0; j < chromosomes.get(i).length; j++)
+                System.out.println(Arrays.toString(chromosomes.get(i)[j]));
+        }
+    }
 }
+
 
